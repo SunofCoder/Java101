1f46820b4e1113cfb21c3f124cf4621d9f4cb6b0
SenemUmac <senemumac@gmail.com>
2022-01-29 00:28:53 +0300

    Kdv.java dosyasi eklendi

---
+++ Kdv.java
@@ -0,0 +1,31 @@
+  import java.util.Scanner;
+
+public class Kdv {
+    public static void main(String[] args) {
+
+        double  ucret , kdv ;
+        double oran = 0.18;
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("ucret giriniz : ");
+        ucret = scanner.nextFloat();
+
+        if(ucret > 1000){
+            oran = 0.08;
+        }
+
+
+        System.out.println("Kdvsiz Fiyati " + ucret );
+
+        kdv   = (ucret * oran);
+        System.out.println("Kdv fiyati" +  kdv );
+
+        double kdvli = kdv+ucret;
+        System.out.println("kvli fiyat " + kdvli);
+        System.out.println("Kdv Orani " + oran );
+
+
+    }
+
+}
